cdb619c3c9f6fa5ec34c4fe5bc020b793eca64d0
Fatlind <fatlindthaci@gmail.com>
2025-10-25 00:31:44 +0200

    Task 22: More loops!

---
+++ Day 3/task_22.py
@@ -0,0 +1,12 @@
+
+# More loops
+my_foods = ["pizza", "sushi", "tacos"]
+friend_foods = ["burger", "pasta", "salad"]
+
+print("My favorite foods are: ")
+for food in my_foods:
+    print(f"- {food}")
+
+print("\nMy friend's favorite foods are: ")
+for food in friend_foods:
+    print(f"- {food}")
