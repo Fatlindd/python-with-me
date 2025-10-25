bb8e1de017566394c07c8af6766ac8fe07db3e17
Fatlind <fatlindthaci@gmail.com>
2025-10-25 20:27:12 +0200

    Task 30: Favorite Fruit!

---
+++ Day 3/task_30.py
@@ -0,0 +1,23 @@
+"""
+Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+Make a list of your three favorite fruits and call it favorite_fruits.
+Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
+"""
+# List of favorite fruits
+favorite_fruits = ["banana", "apple", "grape"]
+
+# Independent if statements
+if "banana" in favorite_fruits:
+   print("You really like bananas!")
+
+if "apple" in favorite_fruits:
+   print("You really like apples!")
+
+if "grape" in favorite_fruits:
+   print("You really like grapes!")
+
+if "mango" in favorite_fruits:
+   print("You really like mangoes!")
+
+if "orange" in favorite_fruits:
+   print("You really like oranges!")
