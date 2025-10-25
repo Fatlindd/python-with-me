fb171157974ed63a728ccebf3e614a6c5ed1c3d7
Fatlind <fatlindthaci@gmail.com>
2025-10-25 20:43:50 +0200

    Task 35: Person!

---
+++ Day 4/task_35.py
@@ -0,0 +1,17 @@
+"""
+Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+"""
+# Define a dictionary with personal info
+person = {
+   "first_name": "Arta",
+   "last_name": "Bajrami",
+   "age": 28,
+   "city": "Prishtina"
+}
+
+
+# Print each piece of information
+print(f"First name: {person['first_name']}")
+print(f"Last name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
