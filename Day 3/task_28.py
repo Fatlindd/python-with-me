f1dfc0389030058985782710509e67ed85fa7e09
Fatlind <fatlindthaci@gmail.com>
2025-10-25 20:08:53 +0200

    Task 28: Alien Colors #3!

---
+++ Day 3/task_28.py
@@ -0,0 +1,38 @@
+"""
+Alien Colors #3: Turn your if-else chain from Exercise 29 into an if- elif-else chain.
+If the alien is green, print a message that the player earned 5 points.
+If the alien is yellow, print a message that the player earned 10 points.
+If the alien is red, print a message that the player earned 15 points.
+Write three versions of this program, making sure each message is printed for the appropriate color alien.
+"""
+# Version 1 - Green alien
+alien_color = 'green'
+
+if alien_color == 'green':
+   print("You just earned 5 points!")
+elif alien_color == 'yellow':
+   print("You just earned 10 points!")
+else:
+   print("You just earned 15 points!")
+
+
+# Version 2 - Yellow alien
+alien_color = 'yellow'
+
+if alien_color == 'green':
+   print("You just earned 5 points!")
+elif alien_color == 'yellow':
+   print("You just earned 10 points!")
+else:
+   print("You just earned 15 points!")
+
+
+# Version 3 - Red alien
+alien_color = 'red'
+
+if alien_color == 'green':
+   print("You just earned 5 points!")
+elif alien_color == 'yellow':
+   print("You just earned 10 points!")
+else:
+   print("You just earned 15 points!")
