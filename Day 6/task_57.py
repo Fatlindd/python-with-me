f4d10713868ef2b4d82d6b6940abbdbbc121f29d
Fatlind <fatlindthaci@gmail.com>
2025-11-09 00:13:32 +0100

    Task 57: Message!

---
+++ Day 6/task_57.py
@@ -0,0 +1,10 @@
+"""
+Message: Write a function called display_message() that prints one sentence telling everyone what you are learning
+about in this chapter. Call the function, and make sure the message displays correctly.
+"""
+def display_message():
+   """Displays a message about what we're learning."""
+   print("In this chapter, we are learning how to define and use functions in Python.")
+
+# Call the function
+display_message()
